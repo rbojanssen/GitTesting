eebae569e76c410aa8aa8989b527c374ff710db6
Ronald Janssen <contact@rbojanssen.com>
2017-12-31 19:49:00 +0100

    Initial commit

---
+++ Branching/Program.vb
@@ -0,0 +1,10 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!" & vbCrLf & "Press any key to close...")
+        'Dim inputString As String = Console.ReadLine()
+        'Console.WriteLine(String.Format("The string you entered: {0}", inputString))
+        Console.ReadKey()
+    End Sub
+End Module
