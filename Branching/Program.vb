381604961918f2e70aab590a36273496b6d77576
Ronald Janssen <contact@rbojanssen.com>
2018-01-01 14:40:12 +0100

    Ask for user input and display the input back

--- Branching/Program.vb
+++ Branching/Program.vb
@@ -2,9 +2,10 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!" & vbCrLf & "Press any key to close...")
-        'Dim inputString As String = Console.ReadLine()
-        'Console.WriteLine(String.Format("The string you entered: {0}", inputString))
+        Console.WriteLine("Hello World!" & vbCrLf)
+        Console.WriteLine("Type some text and hit enter...")
+        Dim inputStr As String = Console.ReadLine()
+        Console.WriteLine(String.Format("You entered the following text: {0}{1}{2}", inputStr, vbCrLf, "Press a key to close this window."))
         Console.ReadKey()
     End Sub
 End Module
