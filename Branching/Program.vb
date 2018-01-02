b35ffabe26197e67ef949c496df73f80169f8ab6
Ronald Janssen <contact@rbojanssen.com>
2018-01-02 07:46:37 +0100

    Check if the user did commit text on the console line.

--- Branching/Program.vb
+++ Branching/Program.vb
@@ -2,10 +2,16 @@ Imports System
 
 Module Program
     Sub Main(args As String())
+        Console.ForegroundColor = ConsoleColor.Cyan
+
         Console.WriteLine("Hello World!" & vbCrLf)
         Console.WriteLine("Type some text and hit enter...")
         Dim inputStr As String = Console.ReadLine()
-        Console.WriteLine(String.Format("You entered the following text: {0}{1}{2}", inputStr, vbCrLf, "Press a key to close this window."))
+        If inputStr.Trim = "" Then
+            Console.WriteLine("Too bad you didn't listen.....bye bye")
+        Else
+            Console.WriteLine(String.Format("You entered the following text: {0}{1}{2}", inputStr, vbCrLf, "Press a key to close this window."))
+        End If
         Console.ReadKey()
     End Sub
 End Module
